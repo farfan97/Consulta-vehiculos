ba7a7a5040bf7ca302c6643cca54170da46dadf2
farfan97 <farfan1976@hotmail.com>
2020-02-06 22:13:26 -0500

    Primer version arreglos iniciales y preparacion de base de datos

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="icon" type="image/png" href="#">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+
+  <body>
+    <div class="container">
+    <nav>
+      <div class="nav-wrapper blue">
+        <a href="#!" class="brand-logo">Acme S.A.</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="index.php">Inicio</a></li>
+          <li><a href="search.php">Buscar</a></li>
+          <li><a href="registro.php">Registro</a></li>
+          <li><a href="act.php">Actualizacion</a></li>
+          <li><a href="report.php">Reporte</a></li>
+        </ul>
+        <ul class="side-nav" id="mobile-demo">
+          <li><a href="index.php">Inicio</a></li>
+          <li><a href="search.php">Buscar</a></li>
+          <li><a href="registro.php">Registro</a></li>
+          <li><a href="act.php">Actualizacion</a></li>
+          <li><a href="report.php">Reporte</a></li>
+        </ul>
+      </div>
+    </nav>
+    </div>
